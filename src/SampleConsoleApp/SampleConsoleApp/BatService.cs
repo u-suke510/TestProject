f7baad09935375c49888394ba1e3b4be4823888f
u-suke510 <usuke510@outlook.jp>
2021-09-12 11:56:55 +0900

    コンソールアプリ追加

---
+++ src/SampleConsoleApp/SampleConsoleApp/BatService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp
+{
+    public interface IBatService
+    {
+    }
+
+    public class BatService : IBatService
+    {
+        private ILogger<BatService> logger;
+
+        public BatService(ILogger<BatService> logger)
+        {
+            this.logger = logger;
+        }
+    }
+}
---
+++ src/SampleConsoleApp/SampleConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace SampleConsoleApp
+{
+    public class Program
+    {
+        private static DateTime procDt;
+        private static IBatService service;
+        private static ILogger<Program> logger;
+
+        public static void Main(string[] args)
+        {
+            procDt = DateTime.Now;
+            Console.WriteLine($"[START] - {procDt:yyyy/MM/dd HH:mm:ss}");
+
+            Setup();
+            Console.WriteLine("Hello World!");
+
+            var endDt = DateTime.Now;
+            Console.WriteLine($"[ END ] - {endDt:yyyy/MM/dd HH:mm:ss} ({endDt.Subtract(procDt).TotalSeconds} sec)");
+        }
+
+        public static void Setup(IBatService batService, ILogger<Program> batLogger)
+        {
+            service = batService;
+            logger = batLogger;
+        }
+
+        public static void Setup()
+        {
+            if (service == null)
+            {
+                var logFactory = LoggerFactory.Create(builder => { });
+                Setup(new BatService(logFactory.CreateLogger<BatService>()), logFactory.CreateLogger<Program>());
+            }
+        }
+    }
+}
